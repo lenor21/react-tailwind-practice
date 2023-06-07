5f5a1e93e4ad841ab779a473f0a98ecba3ceb65a
lenor21 <dejesusronel29@gmail.com>
2023-06-06 19:41:59 -0800

    my firt commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: {
+      port,
+    },
+  },
+});
