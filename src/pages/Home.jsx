caf93ee57ba51b97681467b6d39ce9de82a965a0
lenor21 <dejesusronel29@gmail.com>
2023-06-06 22:00:53 -0800

    fixed p tag element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,24 @@ const Home = () => {
     <div className='h-full w-full'>
       <section className='px-3'>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-          sapiente eius nostrum dicta at obcaecati doloremque laborum,
-          perferendis omnis alias id expedita? Illum ea quia ex voluptate
-          blanditiis rem labore.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sapiente eius nostrum dicta at obcaecati doloremque laborum, perferendis omnis alias
+          id expedita? Illum ea quia ex voluptate blanditiis rem labore.
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-          sapiente eius nostrum dicta at obcaecati doloremque laborum,
-          perferendis omnis alias id expedita? Illum ea quia ex voluptate
-          blanditiis rem labore.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sapiente eius nostrum dicta at obcaecati doloremque laborum, perferendis omnis alias
+          id expedita? Illum ea quia ex voluptate blanditiis rem labore.
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-          sapiente eius nostrum dicta at obcaecati doloremque laborum,
-          perferendis omnis alias id expedita? Illum ea quia ex voluptate
-          blanditiis rem labore.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sapiente eius nostrum dicta at obcaecati doloremque laborum, perferendis omnis alias
+          id expedita? Illum ea quia ex voluptate blanditiis rem labore.
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-          sapiente eius nostrum dicta at obcaecati doloremque laborum,
-          perferendis omnis alias id expedita? Illum ea quia ex voluptate
-          blanditiis rem labore.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sapiente eius nostrum dicta at obcaecati doloremque laborum, perferendis omnis alias
+          id expedita? Illum ea quia ex voluptate blanditiis rem labore.
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-          sapiente eius nostrum dicta at obcaecati doloremque laborum,
-          perferendis omnis alias id expedita? Illum ea quia ex voluptate
-          blanditiis rem labore.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sapiente eius nostrum dicta at obcaecati doloremque laborum, perferendis omnis alias
+          id expedita? Illum ea quia ex voluptate blanditiis rem labore.
         </p>
       </section>
     </div>
